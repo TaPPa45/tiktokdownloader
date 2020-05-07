e42e07b03df807316e1a317cf5bc2c43e0b51561
TaPPa45 <leushkin45@gmail.com>
2020-05-07 07:50:23 -0400

    layout 02

--- tiktokproject/web/views.py
+++ tiktokproject/web/views.py
@@ -5,9 +5,8 @@ from tiktokproject import settings
 
 # Create your views here.
 def index(request):
-    print(settings.COMPRESS_URL)
-    context = {"base_dir":settings.BASE_DIR,"home_dir": settings.HOME_DIR, "rel_media": settings.rel_home('media'), "rel_static":settings.rel_home('media', 'static') }
-    return render(request, 'index.html', context=context)
+    # context = {"base_dir":settings.BASE_DIR,"home_dir": settings.HOME_DIR, "rel_media": settings.rel_home('media'), "rel_static":settings.rel_home('media', 'static') }
+    return render(request, 'index.html', context={})
 
     
     
